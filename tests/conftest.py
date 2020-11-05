f103470b732a056fbb48fa6bba15262b6d628033
José Sánchez-Gallego <gallegoj@uw.edu>
2020-11-04 21:28:41 -0800

    Initial test with docker mocking

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2020-11-04
+# @Filename: conftest.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def mock_docker(mocker):
+    """Mock the docker Python module."""
+
+    yield mocker.patch('docker.DockerClient')
--- tests/test_fliswarm.py
+++ tests/test_fliswarm.py
@@ -6,7 +6,13 @@
 # @Filename: test_fliswarm.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+from fliswarm.nuc import NUC
 
-def test_placeholder():
 
-    assert True
+def test_nuc(mock_docker):
+
+    nuc = NUC('test-nuc', '127.0.0.1')
+    nuc.connect()
+
+    assert nuc.client is not None
+    assert mock_docker.called_once()
